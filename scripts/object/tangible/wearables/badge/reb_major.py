8eb106e71d6123b679db893ce0fc0b274b058dc9
wefi <band0below@hotmail.com>
2014-07-12 18:27:25 -0500

    Added Rebel Requirments for the Badges
    need to do add required rank

--- scripts/object/tangible/wearables/badge/reb_major.py
+++ scripts/object/tangible/wearables/badge/reb_major.py
@@ -3,5 +3,5 @@ import sys
 def setup(core, object):
 	object.setStringAttribute('condition', '100/100')
 	object.setIntAttribute('volume', 1)
-	
+	object.setStringAttribute('required_faction', 'Rebel')
 	return
